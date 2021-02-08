ec53afee85b7760bd920001a9f1e4ba1d576b1b9
agusflorees <78431666+agusflorees@users.noreply.github.com>
2021-02-08 13:59:06 -0300

    TEST_
    Añade comentarios

--- Test.py
+++ Test.py
@@ -1,16 +1,15 @@
 import requests 
 import json
-seller_ID = '179571326'
+seller_id = 179571326
+items = requests.get('https://api.mercadolibre.com/users/'+seller_ID+'/items/search') # guarda en la variable el resultado del llamado a la API
+items_json = items.json() #Pasa el resultado de la consulta a la api a formato JSON
 
-items = requests.get('https://api.mercadolibre.com/users/'+seller_ID+'/items/search') 
-items_json = items.json() #Formatea el resultado de la consulta a la api a formato JSON
 
-
-items_ID= items_json['results']
-log = open ('/archivo.log', 'w' )
-for i in items_ID.count():
-    log.write (items_ID[i]+ "del item,")
-    cat_id = requests.get('https://api.mercadolibre.com/items/'+ items_ID[i])
+items_ID= items_json['results'] # Obtiene el id de la informacion traida desde la API
+log = open ('/archivo.log', 'w' ) 
+for i in items_ID.count(): 
+    log.write (items_ID[i]+ "del item,") 
+    cat_id = requests.get('https://api.mercadolibre.com/items/'+ items_ID[i]) 
     cat_idjson = cat_id.json()
     log.write (cat_idjson['tittle']+ "del item")
     log.write ( cat_idjson['category_id'] + "donde esta publicado")
